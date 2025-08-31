231f1d73f9619fa882cd2ff3bfb36a302fe2ea53
acosta <acostapaez913@gmail.com>
2025-08-31 02:39:54 -0500

    :construction: add cors & morgan configuration

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import { corsConfig } from "./config/cors";
 import { connectDB } from "./config/db";
 import projectRoutes from "./routes/projectRoutes";
+import morgan from "morgan";
 
 dotenv.config();
 connectDB();
 const app = express();
+// CORS
+app.use(cors(corsConfig));
+// Logging
+app.use(morgan("dev"));
+// Read Data Form
 app.use(express.json());
 // Routes
 app.use("/api/projects", projectRoutes);
